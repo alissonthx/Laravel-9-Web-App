1ce765efd607817a2568b796f9c7f7e023e9ac9c
alissonthx <alisson.miquelace@unifcv.edu.br>
2022-05-02 17:00:48 -0300

    feat: Initial layouts in blade

---
+++ Meu-Campeonato/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('content')
+<h1>Home Page</h1>
+<div class="m">
+    <img style="max-width: 60px;" src="/img/trophy.png" alt="">
+    <h1>Meu Campeonato</h1>
+</div>
---
+++ Meu-Campeonato/resources/views/layouts/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-',app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <!-- Application Css -->
+    <link rel="stylesheet" href="/css/style.css">
+    <!-- Font Poppins Google -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;600&display=swap" rel="stylesheet">
+    <!-- Css Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Application Js -->
+    <script src="/js/script.js"></script>
+</head>
+
+<body>
+    @yield('content')
+    <footer>Made it by Álisson Marques &copy;</footer>
+</body>
+
+</html>
