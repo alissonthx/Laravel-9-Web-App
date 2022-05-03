adc85d94e7900e54704f54dc294ec3db8df3d0ba
alissonthx <alisson.miquelace@unifcv.edu.br>
2022-05-02 21:36:59 -0300

    feat: new controllers created

--- Meu-Campeonato/resources/views/classification.blade.php
+++ Meu-Campeonato/resources/views/classification.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
 
-@section('title', 'Home')
+@section('title', 'Classificação')
 
 @section('content')
--- Meu-Campeonato/resources/views/rank.blade.php
+++ Meu-Campeonato/resources/views/rank.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
 
-@section('title', 'Home')
+@section('title', 'Ranking')
 
 @section('content')
